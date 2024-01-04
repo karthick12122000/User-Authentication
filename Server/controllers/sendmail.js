5c3b88c24babfce58a3b69f35d27f12a18a1067e
karthick12122000 <karthicknammakkal@gmail.com>
2024-01-04 21:59:01 +0530

    feat: Installed and configured Node Mailer package

---
+++ Server/controllers/sendmail.js
@@ -0,0 +1,26 @@
+const nodemailer = require("nodemailer");
+const dotenv = require("dotenv");
+dotenv.config();
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.nodeMailerEmail,
+    pass: process.env.nodeMailerPassword,
+  },
+});
+function SendMail(toEmail, subject, content) {
+  const mailOption = {
+    from: process.env.nodeMailerEmail,
+    to: toEmail,
+    subject: subject,
+    html: content,
+  };
+  transporter.sendMail(mailOption, (error, info) => {
+    if (error) {
+      console.log("Error:" + error);
+    } else {
+      console.log("Response:" + info);
+    }
+  });
+}
+module.exports = SendMail;
--- Server/index.js
+++ Server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
 require("dotenv").config();
 // /for access cros platform
 var cors = require("cors");
@@ -7,6 +8,7 @@ app.use(cors({ orgin: "*" }));
 //// for json format
 app.use(express.json());
 var signinrouter = require("./routes/signin");
+
 const port = process.env.PORT || 3000;
 const mongoose = require("mongoose");
 const uri = process.env.MONGODB_CONNECT_URI;
@@ -18,7 +20,9 @@ mongoose.connection.on("connected", () => {
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
 app.use("/signin", signinrouter);
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
