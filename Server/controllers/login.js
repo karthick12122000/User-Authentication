835da402838244b3402b21cfff7c8ee6d46ec671
karthick12122000 <karthicknammakkal@gmail.com>
2024-01-09 22:50:55 +0530

    feat: Implement Authentication User Controller

--- Server/controllers/login.js
+++ Server/controllers/login.js
@@ -1,4 +1,9 @@
 const users = require("../models/user.js");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+const { response } = require("express");
+dotenv.config();
 async function CheckUser(email) {
   try {
     var user = await users.findOne({ email: email });
@@ -11,4 +16,32 @@ async function CheckUser(email) {
     return e;
   }
 }
-module.exports = { CheckUser };
+async function AuthenticateUser(email, password) {
+  try {
+    var userCheck = await users.findOne({ email: email });
+    if (userCheck) {
+      var validatepassword = await bcrypt.compare(password, userCheck.password);
+      if (validatepassword) {
+        const token = jwt.sign(email, process.env.loginsecret_token);
+        const response = {
+          id: userCheck.id,
+          name: userCheck.name,
+          email: userCheck.email,
+          token: token,
+          status: true,
+        };
+        await users.findOneAndUpdate(
+          { email: userCheck.email },
+          { $set: { token: token } },
+          { new: true }
+        );
+        return response;
+      }
+    }
+    return "Invalid user name and password";
+  } catch (error) {
+    console.log(error);
+    return "Server busy";
+  }
+}
+module.exports = { CheckUser, AuthenticateUser };
--- Server/routes/login.js
+++ Server/routes/login.js
@@ -1,4 +1,9 @@
 const express = require("express");
 const router = express.Router();
-router.post("/", async (res, req) => {});
+const { AuthenticateUser } = require("../controllers/login");
+router.post("/", async (req, res) => {
+  const { email, password } = req.body;
+  var loginauth = await AuthenticateUser(email, password);
+  res.status(200).send(loginauth);
+});
 module.exports = router;
