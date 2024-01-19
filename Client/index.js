9b770b2990ab2836369fda574c246c82b5ced498
karthick12122000 <karthicknammakkal@gmail.com>
2024-01-19 22:22:03 +0530

    wip: form validation

--- Client/index.js
+++ Client/index.js
@@ -1,5 +1,23 @@
 import API_URI from "./config/global.js";
 console.log(API_URI);
-// console.log(window.API_URI);
-
-console.log("hi");
+function validateForm() {
+  const name = document.getElementById("floatingName");
+  if (name.value === "") {
+    alert("Name should not be empty!");
+  }
+  const emai = document.getElementById("floatingInput");
+  if (emai.value === "") {
+    alert("Emai should not be empty!");
+  }
+  const password = document.getElementById("floatingPassword");
+  if (password.value === "") {
+    alert("Password should not be empty!");
+  }
+  const confrimPassword = document.getElementById("floatingConfrimPassword");
+  if (confrimPassword.value === "") {
+    alert("Confrim Password should not be empty!");
+  }
+}
+document.getElementById("signup").addEventListener("click", () => {
+  validateForm();
+});
